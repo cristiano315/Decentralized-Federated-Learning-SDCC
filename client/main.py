cb59a7d81cec3e1642783a8d13e54368f0db44cf
Cristiano315 <cristiano.pera315@gmail.com>
2026-04-10 00:08:35 +0200

    Added a dummy main for the client,  and a requirements file. Added a proto file for grpc with a makefile for compiling it.

---
+++ client/main.py
@@ -0,0 +1,15 @@
+import grpc
+
+def main():
+    # Dummy main, to implement
+    # Configuration
+    #1 Start gRPC server
+    #2 Register to service registry
+    #3 Prepare data
+    #4 Discovery
+    #5 Training loop
+    
+    print("Client is running.")
+
+if __name__ == "__main__":
+    main()
