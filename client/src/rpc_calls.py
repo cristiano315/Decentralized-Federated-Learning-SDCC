87d2166971b8889d943e8b35c27319a4cf45ad34
Cristiano315 <cristiano.pera315@gmail.com>
2026-07-19 16:11:51 +0200

    Added traffic measuring

--- client/src/rpc_calls.py
+++ client/src/rpc_calls.py
@@ -146,7 +146,7 @@ def send_weights_to_peer(peer_ip: str, peer_port: int, payload: federated_pb2.We
     """
     Client-side gossip function: Sends local weights to a specific peer.
     """
-    print(f"[RPC] Sending weights to peer at {peer_ip}:{peer_port}...")
+    print(f"[RPC] Sending weights with size: {len(payload.model_weights)} bytes to peer at {peer_ip}:{peer_port}...")
     peer_address = f"{peer_ip}:{peer_port}"
     try:
         with grpc.insecure_channel(peer_address) as channel:
