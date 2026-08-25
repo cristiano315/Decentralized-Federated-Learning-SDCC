b23d6dd26fcc4dd5dab567bf8784f0f9af44ab88
Alfonso Maiorca <alfonso.maiorca@gmail.com>
2026-08-25 12:22:40 +0200

    Bucket update

--- centralized/src/client.py
+++ centralized/src/client.py
@@ -43,7 +43,7 @@ def run_training_loop(config, global_model, servicer, MY_ID, device, RESPAWNED):
     num_epochs = config['num_epochs']
     
     # 1. Preparazione Dataset
-    bucket_name = "sdcc-dataset-771379920513-us-east-1-an"
+    bucket_name = "sdcc-dataset-264452429750-us-east-1-an"
     s3_key = "all_data_niid_05_keep_3_train_9.json"
     X_train, Mask_train, Y_train, X_val, Mask_val, Y_val = SentimentPyTorch.prepare_dataset(bucket_name, s3_key, num_samples, training_set_percentage, start_index, truncated_training_length, max_samples_per_client=1000, seed=42)
     my_samples = len(Y_train)
